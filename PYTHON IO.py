3988aced74e2c4343e6971f4d1600bc6b3b9a46c
Dickson Amenyo <36271402+dkerobean@users.noreply.github.com>
2022-11-07 16:54:04 +0000

    Delete PYTHON IO.py

--- PYTHON IO.py
+++
@@ -1,8 +0,0 @@
-# with open('C:/Users/dkero/Desktop/BossHack.txt','a') as file:
-#     file.write("\nlomea3")
-
-
-with open('C:/Users/dkero/Desktop/BossHack.txt', 'r') as file2:
-    content = file2.read()
-    print(content)
-
